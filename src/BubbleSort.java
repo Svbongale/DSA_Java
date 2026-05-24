49ddd7a61c4c6caa52a1c8926cd5674286875a5a
Savya Sachi V B <savyanio@gmail.com>
2026-05-24 20:21:08 +0530

    Bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,31 +1,35 @@
-import java.util.Random;
-
 public class BubbleSort {
     public static void main(String[] args) {
         BubbleSortImpl bubbleSortObj = new BubbleSortImpl();
 
-        int[] arr =new int[20];
-        int i = 0;
-        Random random = new Random();
-        while (i < arr.length) {
-            arr[i] = random.nextInt();
-            i++;
-        }
+        int[] arr = {2,5,1,3,7,8,4,8};
+
+        System.out.println("Before sorting");
+        bubbleSortObj.printArray(arr);
+
         bubbleSortObj.bubbleSort(arr);
+
+        System.out.println("After sorting");
+        bubbleSortObj.printArray(arr);
     }
 }
 
 class BubbleSortImpl {
     public void bubbleSort(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            int temp = 0;
-            if ((i < arr.length - 1) && (arr[i] < arr[i+1])) {
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+
+        for (int j = arr.length - 1; j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                int temp = 0;
+                if (arr[i] > arr[i + 1]) {  // reverse condition to change order or sorting
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
             }
         }
+    }
 
+    public void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
--- src/ValidParenthesis.java
+++ src/ValidParenthesis.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class ValidParenthesis {
 
